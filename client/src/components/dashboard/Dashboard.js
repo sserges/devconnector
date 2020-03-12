f0afd7335bfca543a255c9cfe9f6c171f5bd11b5
sserges <agounkeserge@gmail.com>
2020-03-12 12:04:32 +0000

    List Education & Experiences

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -6,6 +6,8 @@ import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
 
 import DashboardActions from './DashboardActions';
+import Experience from './Experience';
+import Education from './Education';
 
 import { getCurrentUserProfile } from '../../actions/profile';
 
@@ -29,6 +31,8 @@ const Dashboard = ({
       {profile !== null ? (
         <Fragment>
           <DashboardActions />
+          <Experience experience={profile.experience} />
+          <Education education={profile.education} />
         </Fragment>
       ) : (
         <Fragment>
